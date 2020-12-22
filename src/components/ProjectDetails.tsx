c79c392089182c66c381a3fef92b9b0e886efb4e
Christopher LaChance <christopher.t.lachance@gmail.com>
2020-12-22 03:21:00 +0000

    Changed to an up arrow.

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -42,7 +42,7 @@ export default class ProjectDetails extends Component<IProps, IState> {
         return (
             <Fragment>
                 <div id="return-arrow" onClick={this.props.ReturnArrowCallback}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>
                 </div>
                 <div id="project">{this.props.ProjectComponent}</div>
                 <div id="source-code">
